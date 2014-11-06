6debe8feabcf5003a71ac2c860b69f403c78af80
angelcastrejon <angel.castrejon@gmail.com>
2014-11-05 21:17:15 -0800

    some more java files may be needed

---
+++ BankerBuddy/platforms/android/ant-build/BankerBuddy-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/angelcastrejon/workspace/phonegap/BankerBuddy/BankerBuddy/platforms/android/ant-build/BankerBuddy-debug-unaligned.apk : \
+/Users/angelcastrejon/workspace/phonegap/BankerBuddy/BankerBuddy/platforms/android/ant-build/BankerBuddy.ap_ \
+/Users/angelcastrejon/workspace/phonegap/BankerBuddy/BankerBuddy/platforms/android/ant-build/classes.dex \
+/Users/angelcastrejon/workspace/phonegap/BankerBuddy/BankerBuddy/platforms/android/CordovaLib/ant-build/classes.jar \
